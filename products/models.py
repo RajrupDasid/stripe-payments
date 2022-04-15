d87f879433993abbcc21efac6d7d2997bb91b7b9
RajrupDasid <rajrupdasid@outlook.com>
2022-04-15 19:41:28 +0530

    updated with flake8 statement

--- products/models.py
+++ products/models.py
@@ -3,9 +3,8 @@ from django.db import models
 
 class Product(models.Model):
 
-    name = models.CharField(max_length = 100)
-    price = models.IntegerField(default = 0) #cents
+    name = models.CharField(max_length=100)
+    price = models.IntegerField(default=0)  # cents
 
     def __str__(self):
         return self.name
-
