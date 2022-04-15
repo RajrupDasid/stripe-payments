36020c39cd328e25a33b8489dd6eab5146137876
RajrupDasid <rajrupdasid@outlook.com>
2022-04-15 20:52:58 +0530

    stripe product checkout sussy implement

--- products/views.py
+++ products/views.py
@@ -1,3 +1,29 @@
-# from django.shortcuts import render
+from django.views import View
+from stripe import *
+from django.conf import settings
 
-# Create your views here.
+
+stripe.api_key=settings.STRIPE_SECRET_KEY
+
+class CreateCheckoutSessionView(View):
+    def post(self,request,*args,**kwargs):
+        checkout_session=stripe.checkout.Session.create(
+                payment_method_types=['card'],
+                line_items=[
+                    {
+                        'price_data':{
+                            'currency' : 'usd',
+                            'unit_amount' : 2000,
+                            'product_data' :{
+                                    'name': 'Stubborn Attachments',
+                                    'images': ['https://www.aquariumofpacific.org/images/news/Japanese_Sea_Nettle14_FLAT.jpg']
+                                },
+                            },
+                            'quantity':1,
+                    },
+                    ],
+                    mode='payment'
+                    success_url=YOUR_DOMAIN + '/success.html',
+                    cancel_url=YOUR_DOMAIN + '/cancel.html'
+                )
+
